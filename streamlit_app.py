18d3990a53bddafc0cd42541ec5b7d690481efc1
Changhui-Kim <chhkim0102@gmail.com>
2022-10-11 22:28:55 +0900

    fix file directory

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,27 +9,13 @@ from matplotlib.dates import DateFormatter
 from datetime import datetime, timedelta
 import plotly.express as px
 
-features_mean = np.load('./features_mean.npy')
-top50_dongs = pd.read_csv('./top50_dongs.csv')
+features_mean = np.load('features_mean.npy')
+top50_dongs = pd.read_csv('top50_dongs.csv')
 top50_dongs.columns = [['id', 'name']]
 
 date_list = [f'{y:02d}-{m:02d}' for y in range(2014, 2023) for m in range(1, 13)][10:-5]
 datetime_list = [datetime.strptime(date_list[i], '%Y-%m') for i in range(len(date_list))]
 
-def draw_graph(features, dong, fidx, start=0, end=93):
-	fig, ax = plt.subplots()
-	dong_name = dong['name']
-	dong_id = int(dong['id'])
-	ax.plot(datetime_list,features[dong_id, range(start, end), fidx])
-	plt.rc('font', family='NanumGothic')
-
-	plt.title(f'dong_{dong_id}_{dong_name}')
-	plt.axvline(x=datetime_list[62], color='red')
-	myFmt = DateFormatter("%Y-%d")
-	ax.xaxis.set_major_formatter(myFmt)
-	fig.autofmt_xdate()
-	fig_html = mpld3.fig_to_html(fig)
-	components.html(fig_html, height=600)
 
 features_1 = features_mean[:,:,1]
 dong_ids = [dong_id[0] for dong_id in top50_dongs['id'].values]
@@ -47,7 +33,3 @@ dong = st.sidebar.selectbox("Select a dong:", dong_list)
 st.header("Number of Airbnbs over time")
 fig = px.line(df[df['name']==dong], x='date', y='num_property', title=dong)
 st.plotly_chart(fig)
-
-
-# for i in range(50):
-#     draw_graph(features_mean, top50_dongs.iloc[i], 1)
