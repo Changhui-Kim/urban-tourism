48fe0985e5f39c0a8757a9a6d957c60aa21f50cc
Changhui-Kim <chhkim0102@gmail.com>
2022-10-11 22:22:24 +0900

    draw plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
+import mpld3
+import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
 from datetime import datetime, timedelta
+import plotly.express as px
 
 features_mean = np.load('./features_mean.npy')
 top50_dongs = pd.read_csv('./top50_dongs.csv')
@@ -14,20 +17,37 @@ date_list = [f'{y:02d}-{m:02d}' for y in range(2014, 2023) for m in range(1, 13)
 datetime_list = [datetime.strptime(date_list[i], '%Y-%m') for i in range(len(date_list))]
 
 def draw_graph(features, dong, fidx, start=0, end=93):
-    fig, ax = plt.subplots()
-    dong_name = dong['name']
-    dong_id = int(dong['id'])
-    ax.plot(datetime_list,features[dong_id, range(start, end), fidx])
-    plt.rc('font', family='NanumGothic')
-    
-    plt.title(f'dong_{dong_id}_{dong_name}')
-    plt.axvline(x=datetime_list[62], color='red')
-    myFmt = DateFormatter("%Y-%d")
-    ax.xaxis.set_major_formatter(myFmt)
+	fig, ax = plt.subplots()
+	dong_name = dong['name']
+	dong_id = int(dong['id'])
+	ax.plot(datetime_list,features[dong_id, range(start, end), fidx])
+	plt.rc('font', family='NanumGothic')
 
-    fig.autofmt_xdate()
-    st.pyplot(fig)
+	plt.title(f'dong_{dong_id}_{dong_name}')
+	plt.axvline(x=datetime_list[62], color='red')
+	myFmt = DateFormatter("%Y-%d")
+	ax.xaxis.set_major_formatter(myFmt)
+	fig.autofmt_xdate()
+	fig_html = mpld3.fig_to_html(fig)
+	components.html(fig_html, height=600)
+
+features_1 = features_mean[:,:,1]
+dong_ids = [dong_id[0] for dong_id in top50_dongs['id'].values]
+features_flat = np.concatenate([features_1[dong_id] for dong_id in dong_ids])
+dong_id_list = [dong_id for dong_id in dong_ids for i in range(features_1.shape[1])]
+dong_name_list = [dong_name[0] for dong_name in top50_dongs['name'].values for i in range(features_1.shape[1])]
+ym_list = date_list * 50
+
+dong_list = [dong_name[0] for dong_name in top50_dongs['name'].values]
+
+dict = {'id':dong_id_list, 'name':dong_name_list, 'date':ym_list, 'num_property':features_flat}
+df = pd.DataFrame(dict)
+
+dong = st.sidebar.selectbox("Select a dong:", dong_list)
+st.header("Number of Airbnbs over time")
+fig = px.line(df[df['name']==dong], x='date', y='num_property', title=dong)
+st.plotly_chart(fig)
 
 
-for i in range(50):
-    draw_graph(features_mean, top50_dongs.iloc[i], 1)
+# for i in range(50):
+#     draw_graph(features_mean, top50_dongs.iloc[i], 1)
